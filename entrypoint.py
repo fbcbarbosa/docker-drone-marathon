5f2e9d7c3e89b54dec0cafadbb5b738f121484f2
Fernando Barbosa <fernando.barbosa@quintoandar.com.br>
2017-09-27 11:42:26 -0300

    Make plugin compatible with drone0.5+

--- entrypoint.py
+++ entrypoint.py
@@ -7,10 +7,12 @@ import requests
 
 if __name__ == "__main__":
     try:
+        # try legacy config (drone 0.4)
         config = json.loads(sys.argv[2]).get("vargs")
         server = config.get("server")
         app = config.get("app_config")
     except IndexError:
+        # else try drone 0.5+ config
         server = os.getenv("PLUGIN_SERVER")
         app = json.loads(os.getenv("PLUGIN_APP_CONFIG"))
 
